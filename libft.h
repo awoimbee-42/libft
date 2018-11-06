01f652ee8f63999e11f1f8c358ab48f5339cead8
Arthur WOIMBEE <awoimbee@e2r13p7.42.fr>
2018-11-06 18:29:21 +0100

    fuck this

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: awoimbee <awoimbee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/23 20:34:49 by awoimbee          #+#    #+#             */
-/*   Updated: 2018/11/06 15:24:23 by awoimbee         ###   ########.fr       */
+/*   Updated: 2018/11/06 18:09:28 by awoimbee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,27 +24,27 @@ typedef struct	s_list
 /*
 ** LIBC functions
 */
-void			*ft_memset (void *s, int c, size_t n);
+void			*ft_memset (void *s, int c, size_t len);
 void			ft_bzero(void *s, size_t n);
-void			*ft_memcpy(void *dest, const void *src, size_t n);
+void			*ft_memcpy(void *dst, const void *src, size_t n);
 void			*ft_memccpy(void *dst, const void *src, int c, size_t n);
-void			*ft_memmove(void *dest, const void *src, size_t n);
+void			*ft_memmove(void *dst, const void *src, size_t len);
 void			*ft_memchr(const void *s, int c, size_t n);
 int				ft_memcmp(const void *s1, const void *s2, size_t n);
 size_t			ft_strlen(const char *s);
-char			*ft_strdup(const char *s);
-char			*ft_strcpy(char *dest, const char *src);
-char			*ft_strncpy(char *dest, const char *src, size_t n);
-char			*ft_strcat(char *dest, const char *src);
-char			*ft_strncat(char *dest, const char *src, size_t n);
+char			*ft_strdup(const char *s1);
+char			*ft_strcpy(char *dst, const char *src);
+char			*ft_strncpy(char *dst, const char *src, size_t len);
+char			*ft_strcat(char *s1, const char *s2);
+char			*ft_strncat(char *s1, const char *s2, size_t n);
 size_t			ft_strlcat(char *dst, const char *src, size_t size);
 char			*ft_strchr(const char *s, int c);
 char			*ft_strrchr(const char *s, int c);
 char			*ft_strstr(const char *haystack, const char *needle);
-char			*ft_strnstr(const char *s1, const char *s2, size_t n);
+char			*ft_strnstr(const char *haystack, const char *needle, size_t len);
 int				ft_strcmp(const char *s1, const char *s2);
 int				ft_strncmp(const char *s1, const char *s2, size_t n);
-int				ft_atoi(const char *nptr);
+int				ft_atoi(const char *str);
 int				ft_isalpha(int c);
 int				ft_isdigit(int c);
 int				ft_isalnum(int c);
