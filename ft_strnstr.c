68ba1d55fbfb96a7212994292fc93398e59f18cc
MagicSowap <arthur.woimbee@gmail.com>
2018-11-13 21:17:54 +0100

    wtf was that

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strnstr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: awoimbee <awoimbee@student.42.fr>          +#+  +:+       +#+        */
+/*   By: arthur <arthur@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/26 13:04:05 by awoimbee          #+#    #+#             */
-/*   Updated: 2018/11/12 11:43:55 by awoimbee         ###   ########.fr       */
+/*   Updated: 2018/11/12 20:15:24 by arthur           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,11 +26,8 @@ static int	is_found(const char *s1, const char *s2, size_t n)
 
 char		*ft_strnstr(const char *s1, const char *s2, size_t n)
 {
-	size_t	i;
-
 	if (*s2 == '\0')
 		return ((char*)s1);
-	i = 0;
 	while (*s1 && n)
 	{
 		if (is_found(s1, s2, n))
