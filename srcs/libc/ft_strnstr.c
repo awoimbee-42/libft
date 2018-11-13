2785c79294d719a9dfa4a40f4c47eeab0ca1da17
MagicSowap <arthur.woimbee@gmail.com>
2018-11-13 21:19:55 +0100

    wtf was that

--- srcs/libc/ft_strnstr.c
+++ srcs/libc/ft_strnstr.c
@@ -26,11 +26,8 @@ static int	is_found(const char *s1, const char *s2, size_t n)
 
 char		*ft_strnstr(const char *s1, const char *s2, size_t n)
 {
-	size_t	i;
-
 	if (*s2 == '\0')
 		return ((char*)s1);
-	i = 0;
 	while (*s1 && n)
 	{
 		if (is_found(s1, s2, n))
