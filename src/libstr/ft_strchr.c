9e7797426427cbd4f10c8fe5104f6a222f948d77
Arthur WOIMBEE <awoimbee@e1r6p9.42.fr>
2019-05-11 00:12:58 +0200

    norme 100%

--- src/libstr/ft_strchr.c
+++ src/libstr/ft_strchr.c
@@ -6,16 +6,19 @@
 /*   By: awoimbee <awoimbee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/26 12:47:06 by awoimbee          #+#    #+#             */
-/*   Updated: 2019/05/07 16:17:58 by cpoirier         ###   ########.fr       */
+/*   Updated: 2019/05/11 00:12:26 by awoimbee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/*
+**	This function basically works like strlen,
+**	So I could bing the same optimizations that I did to ft_strlen to it
+*/
+
 char	*ft_strchr(const char *s, int c)
 {
-	/*if (c == '\0')
-		return (ft_strlen(s) + (char *)s);*/
 	while (*s)
 	{
 		if (*s == (char)c)
--- src/t_garbage/gb_realloc.c
+++ src/t_garbage/gb_realloc.c
@@ -6,7 +6,7 @@
 /*   By: awoimbee <awoimbee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/07 18:36:14 by awoimbee          #+#    #+#             */
-/*   Updated: 2019/05/07 22:37:41 by awoimbee         ###   ########.fr       */
+/*   Updated: 2019/05/11 00:11:02 by awoimbee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,6 @@
 
 void		*gb_realloc(t_garbage *gb, void *ptr, size_t new_size)
 {
-	// void	**i;
 	void	*tmp;
 	size_t	i;
 
